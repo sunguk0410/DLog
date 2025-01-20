74c99ed0fffe335189aadca92a3d494367b29d63
kkcc4104 <kkcc4104@naver.com>
2025-01-20 16:34:41 +0900

    first

---
+++ src/main/java/com/dlog/DLog/DLogApplication.java
@@ -0,0 +1,13 @@
+package com.dlog.DLog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DLogApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DLogApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/dlog/DLog/DLogApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dlog.DLog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DLogApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
